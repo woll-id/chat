937c454138e3f65d0c1dbefafbd2a8689bc727d7
Samuel Rinnetmäki <samuel@iki.fi>
2023-11-28 16:10:05 +0200

    replaced bun.serve with express, added didcomm message handler

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
+import { readFileSync } from 'node:fs'
+import * as yaml from 'js-yaml'
 import { createAgent } from '@veramo/core'
-import { DIDManager } from '@veramo/did-manager'
 import { KeyManager } from '@veramo/key-manager'
 import { KeyManagementSystem, SecretBox } from '@veramo/kms-local'
-import { WebDIDProvider } from '@veramo/did-provider-web'
+import { Entities, KeyStore, DIDStore, PrivateKeyStore, migrations } from '@veramo/data-store'
+import { DataSource } from 'typeorm'
+import { DIDManager } from '@veramo/did-manager'
 import { DIDResolverPlugin } from '@veramo/did-resolver'
 import { Resolver } from 'did-resolver'
 import { getResolver as webDidResolver } from 'web-did-resolver'
-import { Entities, KeyStore, DIDStore, PrivateKeyStore, migrations } from '@veramo/data-store'
-import { DataSource } from 'typeorm'
-import { MessageHandler } from '@veramo/message-handler'
-import { DIDCommMessageHandler } from '@veramo/did-comm'
+import { KeyDIDProvider, getDidKeyResolver } from '@veramo/did-provider-key'
+import { PeerDIDProvider, getResolver } from '@veramo/did-provider-peer'
+import { WebDIDProvider } from '@veramo/did-provider-web'
+import { MessageHandler, Message } from '@veramo/message-handler'
+import { DIDComm, DIDCommMessageHandler } from '@veramo/did-comm'
+import { AgentRouter, ApiSchemaRouter, WebDidDocRouter, MessagingRouter, RequestWithAgentRouter } from '@veramo/remote-server'
+import { bytesToBase58, bytesToMultibase, hexToBytes } from '@veramo/utils'
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+// import session from 'express-session'
 
-const myDidAlias = 'samuelmr.github.io:difhack-company'
-const myDidCommUrl = 'https://company.tiedot.me'
-const my httpPort = 3000
-const myDidAlias = 'samuelmr.github.io:difhack-company'
+const configFile = './agent.yml'
+const configString = readFileSync(configFile, 'utf8')
+const config = yaml.load(configString);
+const baseUrl = config.constants.baseUrl.replace(/\/$/, '')  // omit trailing slash
+const [scheme, empty, host, ...pathParts] = baseUrl.split('/')
+let path = ''
+if (pathParts.length) {
+  path = `/${pathParts.join('/')}`
+}
+const messagingEndpoint = config.server.init[0]['$args'][0].messagingServiceEndpoint
+const messagingPath = `${path}${messagingEndpoint}`
 
-const DATABASE_FILE = 'database.sqlite'
-const KMS_SECRET_KEY = '192315f5fc731222950671b7bc39226c8b8b79b8a35274b6a3e34d808d22b153'
+const myDidAlias = [host, ...pathParts].join(':')
+const didDocPath = path.length == 0 ? '/.well-known/did.json' : `${path}/did.json`
+const schemaPath = `${path}/open-api.json`
 
+const httpPort = parseInt(config.constants.port)
+
+const didCommType = 'application/didcomm-encrypted+json'
+
+const DATABASE_FILE = config.constants.databaseFile
+const KMS_SECRET_KEY = config.constants.dbEncryptionKey
+
+// https://github.com/decentralized-identity/veramo/blob/d89a4dd403942445e1262eabd34be88afa5f9685/packages/remote-server/src/web-did-doc-router.ts#L17C46-L24C2
 const keyTypes = {
-  "Secp256k1": "EcdsaSecp256k1VerificationKey2019"
+  Secp256k1: 'EcdsaSecp256k1VerificationKey2019',
+  Secp256r1: 'EcdsaSecp256r1VerificationKey2019',
+  Ed25519: 'Ed25519VerificationKey2018',
+  X25519: 'X25519KeyAgreementKey2019',
+  Bls12381G1: 'Bls12381G1Key2020',
+  Bls12381G2: 'Bls12381G2Key2020',
 }
 
 let myDid
@@ -31,11 +62,11 @@ const dbConnection = new DataSource({
   synchronize: false,
   migrations,
   migrationsRun: true,
-  logging: ['error', 'info', 'warn'],
+  logging: ['debug', 'error', 'info', 'warn'],
   entities: Entities,
 }).initialize()
 
-export const agent = createAgent({
+const agent = createAgent({
   plugins: [
     new KeyManager({
       store: new KeyStore(dbConnection),
@@ -47,69 +78,207 @@ export const agent = createAgent({
       store: new DIDStore(dbConnection),
       defaultProvider: 'did:web',
       providers: {
-        'did:web': new WebDIDProvider({defaultKms: 'local'})
+        'did:key': new KeyDIDProvider({defaultKms: 'local'}),
+        'did:peer': new PeerDIDProvider({defaultKms: 'local'}),
+        'did:web': new WebDIDProvider({defaultKms: 'local'}),
       },
     }),
     new DIDResolverPlugin({
       resolver: new Resolver({
+        ...getDidKeyResolver(),
+        ...getResolver(),
         ...webDidResolver(),
       }),
     }),
+    new MessageHandler({
+      messageHandlers: [
+        new DIDCommMessageHandler(),
+        // new MyMessageHandler()
+      ],
+    }),
+    new DIDComm()
   ],
 })
 
 async function createDID() {
-  const did = await agent.didManagerCreate({ alias: myDidAlias })
+  const did = await agent.didManagerCreate({ alias: myDidAlias }).catch(console.error)
   console.log(`New did ${myDidAlias} created`)
-  console.log(JSON.stringify(did, null, 2))
+  const msgService = {
+    "id": "#messaging",
+    "type": "DIDCommMessaging",
+    "serviceEndpoint": `${scheme}//${host}${messagingPath}`,
+    "description": "Send and receive messages"
+  }
+  await agent.didManagerAddService({ did: did.did, service: msgService}).catch(console.error)
+  did.services.push(msgService)
+
+  const x2Key = await agent.keyManagerCreate({ kms: 'local', type: 'X25519' })
+  await agent.didManagerAddKey({ did: did.did, key: x2Key }).catch(console.error)
+  did.keys.push(x2Key)
+
   return did
 }
 
-myDid = await agent.didManagerFind({ alias: myDidAlias })
-if (myDid.length == 0) {
-  myDid = await createDID().catch(console.error)
-}
-
-const server = Bun.serve({
-  port: httpPort,
-  async fetch(request) {
-    const didDoc = {
-      "@context": [
-          "https://www.w3.org/ns/did/v1",
-          "https://w3id.org/security/v2",
-          "https://w3id.org/security/suites/secp256k1recovery-2020/v2"
-      ],
-      "id": `did:web:${myDidAlias}`,
-      "verificationMethod": [],
-      "authentication": [],
-      "assertionMethod": [],
-      "keyAgreement": [],
-      "service": [
-        {
-          "id": "#messaging",
-          "type": "DIDCommMessaging",
-          "serviceEndpoint": `${myDidCommUrl}#messaging`,
-          "description": "Send and receive messages"
-        }
-      ]
+function getDidDocument(req, res) {
+  const contexts = new Set(['https://www.w3.org/ns/did/v1'])
+  const verificationMethods = []
+  const authentications = []
+  const assertionMethods = []
+  const didDoc = {
+    "@context": [...contexts],
+    "id": `did:web:${myDidAlias}`,
+    "verificationMethod": [],
+    "authentication": [],
+    "assertionMethod": [],
+    "keyAgreement": [],
+    "service": myDid.services
+  }
+  for (const key of myDid.keys) {
+    const keyId = `did:web:${myDidAlias}#${key.kid}`
+    didDoc.verificationMethod.push({
+      "id": keyId,
+      "type": keyTypes[key.type],
+      "controller": `did:web:${myDidAlias}`,
+      "publicKeyHex": key.publicKeyHex
+    })
+    if (key.type == 'X25519') {
+      didDoc.keyAgreement.push(keyId)
     }
-    for (const key of myDid[0].keys) {
-      const keyId = `did:web:${myDidAlias}#${key.kid}`
-      didDoc.verificationMethod.push({
-        "id": keyId,
-        "type": keyTypes[key.type],
-        "controller": `did:web:${myDidAlias}`,
-        "publicKeyHex": key.publicKeyHex
-      })
+    else {
       didDoc.authentication.push(keyId)
       didDoc.assertionMethod.push(keyId)
     }
-    for (const service of myDid[0].services) {
-      didDoc.service.push(service)
+    // from https://github.com/decentralized-identity/veramo/blob/d89a4dd403942445e1262eabd34be88afa5f9685/packages/remote-server/src/web-did-doc-router.ts#L44C3-L110C4
+    switch (didDoc.verificationMethod.at(-1).type) {
+      case 'EcdsaSecp256k1VerificationKey2019':
+      case 'EcdsaSecp256k1RecoveryMethod2020':
+        contexts.add('https://w3id.org/security/v2')
+        contexts.add('https://w3id.org/security/suites/secp256k1recovery-2020/v2')
+        break
+      case 'Ed25519VerificationKey2018':
+        contexts.add('https://w3id.org/security/suites/ed25519-2018/v1')
+        didDoc.verificationMethod.at(-1).publicKeyBase58 = bytesToBase58(hexToBytes(key.publicKeyHex))
+        delete(didDoc.verificationMethod.at(-1).publicKeyHex)
+        break
+      case 'X25519KeyAgreementKey2019':
+        contexts.add('https://w3id.org/security/suites/x25519-2019/v1')
+        didDoc.verificationMethod.at(-1).publicKeyBase58 = bytesToBase58(hexToBytes(key.publicKeyHex))
+        delete(didDoc.verificationMethod.at(-1).publicKeyHex)
+        break
+      case 'Ed25519VerificationKey2020':
+        contexts.add('https://w3id.org/security/suites/ed25519-2020/v1')
+        didDoc.verificationMethod.at(-1).publicKeyMultibase = bytesToMultibase(hexToBytes(key.publicKeyHex), 'base58btc', 'ed25519-pub')
+        delete(didDoc.verificationMethod.at(-1).publicKeyHex)
+        break
+      case 'X25519KeyAgreementKey2020':
+        contexts.add('https://w3id.org/security/suites/x25519-2020/v1')
+        didDoc.verificationMethod.at(-1).publicKeyMultibase = bytesToMultibase(hexToBytes(key.publicKeyHex), 'base58btc', 'x25519-pub')
+        delete(didDoc.verificationMethod.at(-1).publicKeyHex)
+        break
+      case 'EcdsaSecp256r1VerificationKey2019':
+        contexts.add('https://w3id.org/security/v2')
+        break
+      case 'Bls12381G1Key2020':
+      case 'Bls12381G2Key2020':
+        contexts.add('https://w3id.org/security/bbs/v1')
+        break
+      default:
+        break
     }
-    return new Response(JSON.stringify(didDoc))
-  },
+  }
+  didDoc['@context'] = [...contexts]
+  res.json(didDoc)
+}
+
+async function receiveMessage(req, res) {
+  let json
+  let raw
+  if (req.method == 'GET') {
+    json = req.params
+    raw = JSON.stringify(json)
+  }
+  else if (typeof(req.body) == typeof({})) {
+    json = req.body
+    raw = JSON.stringify(json)
+  }
+  else if (req.headers['content-type'].includes('text/plain')) {
+    raw = req.body
+    try {
+      json = JSON.parse(raw)
+    }
+    catch(e) {
+      console.warn('Invalid JSON')
+      console.log(req.body)
+      console.error(e)
+      res.json(e)
+    }
+  }
+  try {
+    const msg = await agent.handleMessage({raw: raw})
+    console.log(msg)
+    // await agent.dataStoreSaveMessage(msg)
+    const now = new Date()
+    const msgId = now.toISOString()
+    const reply = new Message({metaData: msg.metaData })
+    reply.id = msgId
+    reply.createdAt = now.getTime()
+    reply.threadId = msg.threadId
+    reply.type = msg.type
+    reply.data = { content: 'Viesti' }
+    reply.from = msg.to
+    reply.to = msg.from
+    // console.log(reply)
+    const packedReply = await agent.packDIDCommMessage({
+      packing: 'authcrypt',
+      message: reply,
+    })
+    const returnRouteResponse = msg?.metaData?.find((v) => v.type === 'ReturnRouteResponse')
+    if (returnRouteResponse && returnRouteResponse.value) {
+      const returnMessage = JSON.parse(returnRouteResponse.value)
+      console.log('return path found:')
+      console.log(returnMessage)
+      const result = await agent.sendDIDCommMessage({
+        messageId: msgId,
+        packedReply,
+        recipientDidUrl: msg.from,
+      })
+      // console.log(result)
+      agent.emit('DIDCommV2Message-sent', returnMessage.id)
+      res.json(returnMessage.message)
+    } else if (msg) {
+      res.contentType('text/plain').send(packedReply.message)
+      console.log(`Responded with ${packedReply.message}`)
+    }
+  }
+  catch(e) {
+    console.warn('Error parsing message')
+    console.log(JSON.stringify(json, null, 1))
+    console.error(e)
+  }
+}
+
+const dids = await agent.didManagerFind({ alias: myDidAlias })
+if (dids.length > 0) {
+  myDid = dids[0]
+}
+else {
+  myDid = await createDID().catch(console.error)
+}
+
+const messagingRouter = MessagingRouter({
+  metaData: { type: 'express' },
 })
 
-console.log(`Listening on localhost:${server.port}`)
-
+const app = express()
+app.set('trust proxy', 1)
+app.use(cors())
+app.use(bodyParser.json({type: didCommType}))
+app.use(bodyParser.text({type: 'text/plain'}))
+app.get(path, (req, res) => { res.json(agent.availableMethods()) })
+app.post(messagingPath, receiveMessage)
+app.get(messagingPath, receiveMessage)
+app.get(didDocPath, getDidDocument)
+var server = app.listen(httpPort, (err) => {
+  if (err) { console.error(err) }
+  console.log(`Server running on port ${httpPort}, public address ${baseUrl}`)
+})
